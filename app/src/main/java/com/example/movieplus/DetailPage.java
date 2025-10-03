7677b9e686ec641c0443ee47fe4398c68b91281e
Joe <josephatanga25@gmail.com>
2025-10-02 22:16:06 -0700

    joe

--- app/src/main/java/com/example/movieplus/DetailPage.java
+++ app/src/main/java/com/example/movieplus/DetailPage.java
@@ -3,24 +3,32 @@ package com.example.movieplus;
 import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
-import android.widget.ImageView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.movieplus.R;
+import com.example.movieplus.ui.Favorites.DatabaseHelper;
+import com.example.movieplus.ui.Favorites.FavoritesAdapter;
+import com.example.movieplus.ui.Favorites.Movie;
 
 public class DetailPage extends AppCompatActivity {
 
     TextView Name,Desc,Title,Image;
-
+    //    ImageView imageView;
     WebView webView;
+    private Button favoriteButton;
 
-    ImageView imageView;
-    TextView textView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +40,38 @@ public class DetailPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
 
-
-            });
-
-
+        });
 
 
         Intent intent = getIntent();
         String[] Product = intent.getStringArrayExtra("Product");
 
-        Name = findViewById(R.id.textView12);
-        imageView = findViewById(R.id.detailImage);
-        webView = findViewById(R.id.webView);
+        Name = findViewById(R.id.textView13);
+        Desc = findViewById(R.id.textView14);
+//        Title = findViewById(R.id.textView15);
+//        imageView = findViewById(R.id.detailImage2);
+
+        webView = findViewById(R.id.webView2);
+//        favoriteButton = findViewById(R.id.button5);
+//
+//        favoriteButton.setOnClickListener(v -> {
+//            Intent intent1 = new Intent(DetailPage.this, FragmentActivity.class);
+//            startActivity(intent1);
+//            finish();
+//        });
 
 
+
+
+
+
+
+//        String s = "file:///android_asset/add.php?bc=" + "bc";
+//        webView.loadUrl(s);
+
+
+//        webView.
+//        webView.loadUrl("file:///android_asset/youtube.html");
 
 
 
@@ -54,15 +80,27 @@ public class DetailPage extends AppCompatActivity {
         String Desc = Product[1];
         String Title = Product[2];
         String Image = Product[3];
-        imageView.setImageResource(Integer.parseInt(Image));
-        String url = getIntent().getStringExtra("URL");
-        webView.loadUrl("url");
-//        textView.setText(Name);
-//        textView.setText(Desc);
+        String Url = Product[4];
 
 
 
 
+
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        String html = "<html><body>" +
+                "<iframe width=\"100%\" height=\"100%\" src=\""+Url+"\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write;   encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>" +
+                "</body></html>";
+        String baseUrl = "https://example.com/";
+        webView.loadDataWithBaseURL(baseUrl, html, "text/html", null, baseUrl);
+//        imageView.setImageResource(Integer.parseInt(Image));
+        this.Name.setText(Name);
+        this.Desc.setText(Desc);
+//        this.Title.setText(Title);
 
     }
+
+
 }
